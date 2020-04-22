daa7cd2cb7fbfa461b2e6c2316bb19af5ef882e5
Alexander Kaschuk <58186279+AlexKaschuk@users.noreply.github.com>
2020-04-22 19:53:21 +0300

    Add files via upload

--- FL13_HW6/homework/js/task1.js
+++ FL13_HW6/homework/js/task1.js
@@ -1,15 +1,12 @@
 const MaxPersent = 100;
 let sum = parseFloat(prompt('inputs check number'));
-let persent, x;
-if (!sum || sum < 0) {
+let persent = prompt('inputs tip percentage');
+let x;
+if (!sum || sum < 0 || !persent || persent < 0 || persent > MaxPersent) {
     alert('Invalid input data');
 } else {
-    persent = prompt('inputs tip percentage');
-    if (!persent || persent < 0 || persent > MaxPersent) {
-        alert('Invalid input data');
-    } else {
-        sum = Math.round(sum * MaxPersent) / MaxPersent;
-        x = Math.round(sum * persent) / MaxPersent;
-        alert(`check number: ${sum}\ntip: ${persent} %\ntip amount: ${x}\ntotal summ:  ${sum + x}  ye`);
-    }
+    sum = Math.round(sum * MaxPersent) / MaxPersent;
+    x = Math.round(sum * persent) / MaxPersent;
+    alert(`check number: ${sum}\ntip: ${persent} %\ntip amount: ${x}\ntotal summ:  ${sum + x}  ye`);
+
 }
